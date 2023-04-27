149de5a8d9914c8b120a34f8ba7b5cb9a1e98505
chetlasairam <chetlasai168@gmail.com>
2023-04-26 23:44:08 -0400

    portfolio
    Portfolio Sai Ram

---
+++ form_process.php
@@ -0,0 +1,83 @@
+<?php
+
+$name_error = $email_error = $phone_error = $url_error ="";
+$name = $email = $phone = $url = $success="";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(empty($_POST["name"])){
+        $name_error="Name is required";
+    } else{
+        $name = test_input($_POST["name"]);
+
+        if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+            $name_error = "Only letters and white space allowed";
+          }
+    }
+
+    if(empty($_POST["email"])){
+        $email_error="Email is required";
+    } else{
+        $email = test_input($_POST["email"]);
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_error = "Invalid email format";
+          }
+    }
+
+    if(empty($_POST["phone"])){
+        $phone_error="Phone is required";
+    } else{
+        $phone = test_input($_POST["phone"]);
+
+        if (!preg_match("/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i",$phone)) {
+            $phone_error = "Invalid phone number";
+          }
+    }
+
+    if(empty($_POST["url"])){
+        $url_error="";
+    } else{
+        $url = test_input($_POST["url"]);
+
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url)) {
+            $url_error = "Invalid URL";
+          }
+    }
+
+
+    if(empty($_POST["message"])){
+        $message="";
+    }else{
+        $message=test_input($_POST["message"]);
+    }
+
+    if($name_error == '' and $email_error == '' and $phone_error == '' and $url_error ==''){
+        $message_body='';
+        unset($_POST['submit']);
+        foreach($_POST as $key => $value){
+            $message_body .= "$key:$value\n";
+        }
+
+        $to = 'chetlasai168@gmail.com'; 
+        $subject = 'Contact form submitted';
+        if (mail ($to, $subject, $message)) { 
+            $success="Message sent";
+            $name = $email = $phone = $url = $success="";
+        } 
+
+    }
+
+
+}
+
+function test_input($data){
+    $data=trim($data);
+    $data=stripslashes($data);
+    $data=htmlspecialchars($data);
+    return $data;
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sai Ram</title>
+    <link rel="shortcut icon" href="./img/icon.png" type="image/x-icon" />
+    
+
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Plaster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Aclonica&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!--  Bootstrap css file  -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!--  font awesome icons  -->
+    <link rel="stylesheet" href="./css/all.min.css">
+
+    <!--icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!--  custom css file  -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!--  custom responsive file  -->
+    <link rel="stylesheet" href="./css/responsive.css">
+
+    
+
+</head>
+
+<body>   
+    <!--main-->
+    <div class="main">
+
+    
+    
+
+    
+        <!--Header-->
+    <div class="header-area sticky-top">
+        <nav class="navbar navbar-expand-lg navbar-dark  ">
+            <a class="navbar-brand-logo" href="#">S</a>
+            <a class="navbar-brand-name" href="#">CHETLA</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mr-auto"></div>
+              <ul class="navbar-nav ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#about">about <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#Experience">Experience <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projects">projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=chetlasai168@gmail.com">hire me</a>
+                </li>
+                <li class="nav-item">
+                    <button class="btn  my-2 my-sm-3" onclick="window.open('./download/Resume.pdf')"  type="submit">Download CV</button>
+                </li>
+                
+                
+                </ul>
+              
+            </div>
+          </nav>
+    </div>
+
+
+
+    <!--End Header-->
+
+  <!--intro-->
+    <div class="intro" id="intro">
+        
+        <div class="card row">
+
+            <div class="name">
+                <h2 class="title-text hey">Hey!</h2>
+                <h1 class="title-text text-uppercase">I am sai ram chetla</h1>
+                <h5 class="title-text ">
+                    <span class="skills">-Web development</span>
+                    <span class="skills">-Python</span>
+                    <span class="skills">-Flutter</span>
+                </h5>
+            </div>
+
+            <div class="dp">
+
+                <!--<div class="mask"></div>-->
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <!--end intro-->
+
+    <!--Start about-->
+
+    <div class="about" id="about">
+        <h1 class="banner text-uppercase">about</h1>
+        <p class="para">
+            I am a hardworking engineering graduate from hyderabad. I am good at designing and developing websites. I also developed a portfolio for myself. 
+            I even have basic knowledge about developing android and ios apps using Flutter. I have developed a couple of real time apps.
+            I worked on Image processing projects too. I'm also good at electronics. I've done lots of projects using Arduino and different sensors.
+        </p>
+        <button class="btn " onclick="window.open('./download/Resume.pdf')" type="submit">Download CV</button>
+    </div>
+
+    <!--end about-->
+
+    <!--Start Experience-->
+
+    <div class="about" id="Experience">
+        <h1 class="banner text-uppercase">Experience</h1>
+        <ul>
+        <h2 class="text-uppercase">Bartleby</h2>
+  <li>
+    
+    Worked as a Subject Teaching Expert in Bartleby.
+  </li>
+  <li>
+    Solver over 1000 Questions.
+  </li>
+</ul>
+        
+    </div>
+
+    <!--end Experience-->
+
+
+    <!--Start projects-->
+    <div class="projects" id="projects">
+        <h1 class="banner text-uppercase">projects</h1>
+    
+        <div class="hex-div">
+    <ul class="honeycomb">
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/walkingrobot.jpg">
+            <div class="honeycomb-cell_title">Walking Robot</div>
+        </li>
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/painpointer.jpg">
+            <div class="honeycomb-cell_title">Pain Pointer</div>
+        </li>
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/switchboard.jpg">
+            <div class="honeycomb-cell_title">Smart SwitchBoard</div>
+        </li>
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/husk.png">
+            <div class="honeycomb-cell_title">Husk<small>Chatting App</small></div>
+        </li>
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/getdish.jpg">
+            <div class="honeycomb-cell_title">GetDish<small>Food App</small></div>
+        </li>
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/sudokusolver.jpg">
+            <div class="honeycomb-cell_title">Sudoku Solver</small></div>
+        </li>
+        <li class="honeycomb-cell">
+            <img class="honeycomb-cell_img" src="./img/intro_pic/heyarvin.jpg">
+            <div class="honeycomb-cell_title">Hey Arvin</div>
+        </li>
+        <li class="honeycomb-cell honeycomb_Hidden">
+        </li>
+    </ul>
+</div>
+</div>
+    <!--end projects-->
+
+
+    <!--Start services-->
+    <div class="services" id="services">
+        <h1 class="banner text-uppercase">services</h1>
+
+        <div class="hex-div">
+            <ul class="honeycomb">
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/webdesigning.png">
+                    <div class="honeycomb-cell_title"></div>
+                    <div class="honeycomb-cell_theory down">Web Designing<small>We convert your idea into a creative website design</small></div>
+
+                </li>
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/webdevelopment.png">
+                    <div class="honeycomb-cell_title">Graphic Design</div>
+                    <div class="honeycomb-cell_theory down">Web Development<small>We convert a design in to a interactive website</small></div>
+
+                </li>
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/appdev.png">
+                    <div class="honeycomb-cell_title">Illustration</div>
+                    <div class="honeycomb-cell_theory down">App Development<small>We develop beautiful realtime apps</small></div>
+                </li>
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/photoshop.jpg">
+                    <div class="honeycomb-cell_title">Motion Graphics</div>
+                    <div class="honeycomb-cell_theory down">Photoshop<small>Cover pages for novels and pictures are also edited here</small></div>
+                </li>
+
+                <li class="honeycomb-cell honeycomb_Hidden">
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!--end services-->
+
+
+    <!--Start languages-->
+
+    <div class="languages" id="languages">
+
+        <h1 class="banner text-uppercase">languages</h1>
+
+        <div class="hex-div">
+            <ul class="honeycomb">
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/c.png">
+                    <div class="honeycomb-cell_title">C</div>
+                    <div class="honeycomb-cell_theory down">C<small>Language to learn basic programming</small></div>
+
+                </li>
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/pyt.jpg">
+                    <div class="honeycomb-cell_title">Python</div>
+                    <div class="honeycomb-cell_theory down">Python<small>Tool for Machine learning, Image processing</small></div>
+
+                </li>
+                <li class="honeycomb-cell honeycomb-cell-vertical">
+                    <img class="honeycomb-cell_img" src="./img/intro_pic/dart.jpg">
+                    <div class="honeycomb-cell_title">Dart</div>
+                    <div class="honeycomb-cell_theory down">Dart<small>Android and IOS apps are developed using this</small></div>
+                </li>
+                
+
+                <li class="honeycomb-cell honeycomb_Hidden">
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    
+
+    <!--end languages-->
+
+
+    <!--Contact PHP-->
+
+    <?php include('form_process.php'); ?>
+
+    <!--End Contact PHP-->
+    
+
+    <!--Start contact-->
+    
+    <div class="contact">
+        <div class="container">  
+            <form id="contact" action="<?= $_SERVER['PHP_SELF'];?>" method="post">
+              <h3>Quick Contact</h3>
+              <h4>Contact us today, and get reply with in 24 hours!</h4>
+              <fieldset>
+                <input placeholder="Your name" type="text" tabindex="1" name="name" value="<?= $name?>" >
+                <span class="error"><?= $name_error ?></span>
+              </fieldset>
+              <fieldset>
+                <input placeholder="Your Email Address" type="text" name="email" value="<?= $email?>" tabindex="2">
+                <span class="error"><?= $email_error ?></span>
+              </fieldset>
+              <fieldset>
+                <input placeholder="Your Phone Number" type="text" name="phone" value="<?= $phone?>" tabindex="3" >
+                <span class="error"><?= $phone_error ?></span>
+              </fieldset>
+              <fieldset>
+                <input placeholder="Your Web Site starts with http://" type="text" name="url" value="<?= $url?>" tabindex="4" >
+                <span class="error"><?= $url_error ?></span>
+              </fieldset>
+              <fieldset>
+                <textarea placeholder="Type your Message Here...." type="text" name="message" value="<?= $message?>" tabindex="5" ></textarea>
+                
+              </fieldset>
+              <fieldset>
+                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+              </fieldset>
+              <div class="success"><?= $success ?></div>
+            </form>
+           
+            
+          </div>
+    </div>
+
+    <!--end contact-->
+
+
+    
+
+    <!--Start icons-->
+    <div class="icons">
+        <a href="https://www.facebook.com/chetla.sairam" class="fa fa-facebook"></a>
+        <a href="https://github.com/chetlasairam" class="fa fa-github"></a>
+        <a href="https://www.linkedin.com/in/sai-ram-chetla/" class="fa fa-linkedin"></a>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=chetlasai168@gmail.com" target="_blank" class="fa fa-google"></a>
+    </div>      
+    
+        <!--end icons-->
+
+
+
+
+
+
+    <!--  Jquery js file  -->
+    <script src="./js/jquery.3.4.1.js"></script>
+    
+    <!--  Bootstrap js file  -->
+    <script src="./js/bootstrap.min.js"></script>
+    
+    <!--  custom js file  -->
+    <script src="./js/main.js"></script>
+    
+    
+    </div>
+<!--end main-->
+</body>
+
+</html>
